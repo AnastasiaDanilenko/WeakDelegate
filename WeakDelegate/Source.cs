0a006d24ebabeb172d4fe975e7ecdd4d3ab0e0c6
AnastasiaDanilenko <451005asya@gmail.com>
2016-09-26 00:13:19 +0300

    Source class corrected

--- WeakDelegate/Source.cs
+++ WeakDelegate/Source.cs
@@ -9,11 +9,27 @@ namespace WeakDelegate
     public class Source
     {
 
+        public event Action<int> OneArgument;
+        public event Action<int, double> TwoArguments;
+        public event Action<int, double, int> ThreeArguments;
+        public event Action<int, int, int, int> FourArguments;
 
-
-        public Source()
+        public int WhenCalled()
         {
+            int Info =  DateTime.Now.Second;
+            return Info;
+        }
 
+        public void Events()
+        {
+            if (OneArgument != null)
+                OneArgument.Invoke(WhenCalled());
+            if (TwoArguments != null)
+                TwoArguments.Invoke(WhenCalled(), WhenCalled());
+            if (ThreeArguments != null)
+                ThreeArguments.Invoke(WhenCalled(), WhenCalled(), WhenCalled());
+            if (FourArguments != null)
+                FourArguments.Invoke(WhenCalled(), WhenCalled(), WhenCalled(), WhenCalled());
         }
     }
 }
--- WeakDelegate/Wrapper.cs
+++ WeakDelegate/Wrapper.cs
@@ -11,11 +11,22 @@ namespace WeakDelegate
     {
         public WeakReference weakref = null;
         public MethodInfo mi;
+        public Delegate someinf
+        {
+            get;
+            private set;
+        }
 
         public Wrapper(Delegate somedelegate)
         {
             weakref = new WeakReference(somedelegate.Target);
             mi = somedelegate.Method;
+        }
+
+        public void ReturnDelegate()
+        {
+            if (weakref.IsAlive)
+                someinf = mi.CreateDelegate(mi.GetType());
         }
     }
 }
