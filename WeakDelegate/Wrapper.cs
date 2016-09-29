01a6e624780f67c1d3d7b0caaf970f8415952f96
AnastasiaDanilenko <451005asya@gmail.com>
2016-09-29 10:33:31 +0300

    Expression added

--- WeakDelegate/Wrapper.cs
+++ WeakDelegate/Wrapper.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -27,6 +25,40 @@ namespace WeakDelegate
         {
             if (weakref.IsAlive)
                 someinf = mi.CreateDelegate(mi.GetType());
+        }
+
+        public Delegate GetDelegate()
+        {
+            ParameterExpression[] paramArr = GetParametersExpression(mi);
+            Expression targetObjectExpression = GetPropertyExpression(Expression.Constant(weakref), "Target", weakref.Target.GetType());
+            Expression targetMethodInvoke = Expression.Call(targetObjectExpression, mi, paramArr);
+            Expression conditionExpression = Expression.NotEqual(targetObjectExpression, Expression.Constant(null));
+            Expression ifExpression = Expression.IfThen(conditionExpression, targetMethodInvoke);
+            LambdaExpression labmda = Expression.Lambda(ifExpression, paramArr);
+            return labmda.Compile();
+        }
+
+        private ParameterExpression[] GetParametersExpression(MethodInfo method)
+        {
+            ParameterInfo[] eventHandlerArgsInfo = method.GetParameters();
+            ParameterExpression[] eventHandlerArgsExpressionMassive = new ParameterExpression[eventHandlerArgsInfo.Length];
+            for (int i = 0; i < eventHandlerArgsInfo.Length; i++)
+            {
+                eventHandlerArgsExpressionMassive[i] = ParameterExpression.Parameter(eventHandlerArgsInfo[i].ParameterType);
+            }
+            return eventHandlerArgsExpressionMassive;
+        }
+
+        private Expression GetPropertyExpression(Expression declaryingObjectExpression, String propertyName, Type typeToCastProperty = null)
+        {
+            Type declaryingClassType = declaryingObjectExpression.Type;
+            PropertyInfo targetPropertyInfo = declaryingClassType.GetProperty(propertyName);
+            Expression targetObjectExpression = Expression.Property(declaryingObjectExpression, targetPropertyInfo);
+            if (typeToCastProperty != null)
+            {
+                targetObjectExpression = Expression.Convert(targetObjectExpression, typeToCastProperty);
+            }
+            return targetObjectExpression;
         }
     }
 }
